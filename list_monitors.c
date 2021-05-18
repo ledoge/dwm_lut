d0cb08ca6446518324d106bc4b981fc4a2651b65
ledoge <8040316+ledoge@users.noreply.github.com>
2021-05-18 04:07:49 +0200

    Add primitive application for listing monitors

---
+++ list_monitors.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <windows.h>
+
+BOOL MonitorEnumProc(HMONITOR hMonitor, HDC hdcMonitor, LPRECT lprcMonitor, LPARAM dwData) {
+    MONITORINFOEX monitorInfo;
+    monitorInfo.cbSize = sizeof(MONITORINFOEX);
+    GetMonitorInfo(hMonitor, (MONITORINFO *) &monitorInfo);
+
+    if (!(monitorInfo.dwFlags & DISPLAY_DEVICE_MIRRORING_DRIVER)) {
+        RECT* rect = &monitorInfo.rcMonitor;
+        printf("%s", monitorInfo.szDevice);
+        if (rect->left == 0 && rect->top == 0) {
+            printf(" (primary monitor)");
+        }
+        printf("\n");
+        printf("Rectangle: (%d, %d), (%d, %d)\n", rect->left, rect->top, rect->right, rect->bottom);
+        printf("Resolution: %dx%d\n", rect->right - rect->left, rect->bottom - rect->top);
+        printf("Filename: %d_%d.cube\n", rect->left, rect->top);
+        printf("\n");
+    }
+
+    return TRUE;
+}
+
+int main() {
+    EnumDisplayMonitors(NULL, NULL, MonitorEnumProc, 0);
+    system("pause");
+}
