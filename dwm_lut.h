225173ab7517f55cf884dda42454b857a0cde31f
ledoge <8040316+ledoge@users.noreply.github.com>
2021-05-20 02:19:00 +0200

    Add define for LUT file extension

--- dwm_lut.h
+++ dwm_lut.h
@@ -1,6 +1,7 @@
 #define BASEPATH "%SYSTEMROOT%\\Temp\\"
 #define DLL_NAME "dwm_lut.dll"
+#define LUT_FILEEXT ".cube"
 #define LUT_FOLDER "luts"
-#define LUT_NAME "lut.cube"
+#define LUT_NAME "lut" LUT_FILEEXT
 #define LUT_SIZE 65
 #define MAX_LUTS 32
