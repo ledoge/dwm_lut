42fbd68c15d08104c582de465b180018e018f541
ledoge <8040316+ledoge@users.noreply.github.com>
2021-07-28 23:20:38 +0200

    Add some command line arguments (and a tray icon)

--- DwmLutGUI/DwmLutGUI/MainWindow.xaml.cs
+++ DwmLutGUI/DwmLutGUI/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +15,74 @@ namespace DwmLutGUI
 
         public MainWindow()
         {
+            if (Process.GetProcessesByName(Process.GetCurrentProcess().ProcessName).Length > 1)
+            {
+                MessageBox.Show("Already running!");
+                Close();
+                return;
+            }
+
             InitializeComponent();
             _viewModel = (MainViewModel)DataContext;
             _applyOnCooldown = false;
+
+            var args = Environment.GetCommandLineArgs().ToList();
+            args.RemoveAt(0);
+
+            if (args.Contains("-apply"))
+            {
+                Apply_Click(null, null);
+            }
+            else if (args.Contains("-disable"))
+            {
+                Disable_Click(null, null);
+            }
+
+            if (args.Contains("-minimize"))
+            {
+                WindowState = System.Windows.WindowState.Minimized;
+                Hide();
+            }
+            else if (args.Contains("-exit"))
+            {
+                Close();
+                return;
+            }
+
+            var notifyIcon = new NotifyIcon();
+            var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("DwmLutGUI.smile.ico");
+            notifyIcon.Icon = new System.Drawing.Icon(stream);
+            notifyIcon.Visible = true;
+            notifyIcon.DoubleClick +=
+                delegate
+                {
+                    Show();
+                    WindowState = System.Windows.WindowState.Normal;
+                };
+
+            var contextMenu = new ContextMenu();
+            var menuItem = new MenuItem();
+            contextMenu.MenuItems.Add(menuItem);
+
+            menuItem.Index = 0;
+            menuItem.Text = "Exit";
+            menuItem.Click += delegate { Close(); };
+
+            notifyIcon.ContextMenu = contextMenu;
+
+            notifyIcon.Text = Assembly.GetEntryAssembly().GetName().Name;
+
+            Closed += delegate { notifyIcon.Dispose(); };
+        }
+
+        protected override void OnStateChanged(EventArgs e)
+        {
+            if (WindowState == System.Windows.WindowState.Minimized)
+            {
+                Hide();
+            }
+
+            base.OnStateChanged(e);
         }
 
         private static string BrowseLuts()
